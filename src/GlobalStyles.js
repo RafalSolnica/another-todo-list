3ff4a867e27f7a7eedc144bf6887724e6374d888
Rafał Solnica <granadag99@gmail.com>
2023-11-20 17:27:41 +0100

    created globalstyles and theme

---
+++ src/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+  }
+
+  *,*::before,*::after{
+    box-sizing: inherit;
+  }
+
+  :root{
+    color-scheme: dark light;
+  }
+
+  body{
+    font-family:"Lato";
+  }
+`;
