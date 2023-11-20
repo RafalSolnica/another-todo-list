5ec17d9d457d9c219be81cc01851357b3b43d8d3
Rafał Solnica <granadag99@gmail.com>
2023-11-20 18:14:15 +0100

    created new styled components inside form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -12,6 +12,19 @@ export const Row = styled.div`
   gap: 0.1rem;
 `;
 
+export const Input = styled.input`
+  outline: none;
+  border: 1px solid hsl(200, 100%, 40%);
+  background: hsl(200, 100%, 30%);
+  border-radius: 0.25em;
+  padding: 0.25em 0.5em;
+  color: hsl(200, 100%, 90%);
+
+  &:focus {
+    border: 1px solid hsl(200, 100%, 70%);
+  }
+`;
+
 export const Button = styled.button`
   background: hsl(200, 100%, 50%, 0.1);
   border: 1px solid hsl(200, 100%, 50%);
