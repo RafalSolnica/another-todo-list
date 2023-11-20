4b6923bd9f213d6bfd32c9b24e3a84c30c3f9ceb
Rafał Solnica <granadag99@gmail.com>
2023-11-20 21:54:44 +0100

    moved some functionality into TaskItem component

--- src/TaskList/index.js
+++ src/TaskList/index.js
@@ -1,42 +1,13 @@
-import { StyledList, ListItem, RemoveButton } from "./styled";
+import { StyledList } from "./styled";
+import { TaskItem } from "./TaskItem";
 
 export const TaskList = ({ tasks, setTasks }) => {
-  const toggletask = (id, completed) => {
-    setTasks((currentTasks) => {
-      return currentTasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed };
-        }
-
-        return task;
-      });
-    });
-  };
-
-  const deleteTask = (id) => {
-    setTasks((currentTasks) => {
-      return currentTasks.filter((task) => task.id !== id);
-    });
-  };
-
   return (
     <StyledList>
       {tasks.length === 0 && "Brak zadań"}
       {tasks.map((task) => {
         return (
-          <ListItem key={task.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={(e) => toggletask(task.id, e.target.checked)}
-              />
-              {task.title}
-              <RemoveButton onClick={() => deleteTask(task.id)}>
-                Delete
-              </RemoveButton>
-            </label>
-          </ListItem>
+          <TaskItem key={task.id} task={task} setTasks={setTasks}></TaskItem>
         );
       })}
     </StyledList>
