5d51350082552a5b428c4f2a165ee8137045cfa4
Rafał Solnica <granadag99@gmail.com>
2023-11-20 21:54:29 +0100

    created new TaskItem component

---
+++ src/TaskList/TaskItem/index.js
@@ -0,0 +1,35 @@
+import { StyledItem, RemoveButton } from "./styled";
+
+export const TaskItem = ({ task, setTasks }) => {
+  const toggletask = (id, completed) => {
+    setTasks((currentTasks) => {
+      return currentTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed };
+        }
+
+        return task;
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((currentTasks) => {
+      return currentTasks.filter((task) => task.id !== id);
+    });
+  };
+
+  return (
+    <StyledItem key={task.id}>
+      <label>
+        <input
+          type="checkbox"
+          checked={task.completed}
+          onChange={(e) => toggletask(task.id, e.target.checked)}
+        />
+        {task.title}
+        <RemoveButton onClick={() => deleteTask(task.id)}>Delete</RemoveButton>
+      </label>
+    </StyledItem>
+  );
+};
---
+++ src/TaskList/TaskItem/styled.js
@@ -0,0 +1,66 @@
+import { styled } from "styled-components";
+
+export const StyledItem = styled.li`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+
+  &:has(input:checked) label {
+    color: hsl(200, 20%, 40%);
+  }
+
+  & label {
+    display: flex;
+    gap: 0.4rem;
+    cursor: pointer;
+    align-items: center;
+  }
+
+  & input {
+    outline: none;
+    width: 0;
+    height: 0;
+    appearance: none;
+    pointer-events: none;
+    position: absolute;
+  }
+
+  & label::before {
+    content: "";
+    display: block;
+    width: 0.9rem;
+    height: 0.9rem;
+    background: hsl(200, 100%, 90%);
+    border-radius: 0.25em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & li label:hover::before {
+    background: hsl(200, 100%, 80%);
+  }
+
+  &:has(input:checked) label::before {
+    content: "✔";
+    background: hsl(200, 100%, 40%);
+    color: hsl(200, 100%, 90%);
+    font-size: 0.75rem;
+    font-weight: bold;
+  }
+
+  &:has(input:checked) label:hover::before {
+    background: hsl(200, 100%, 30%);
+  }
+`;
+
+export const RemoveButton = styled.button`
+  background: hsl(0, 100%, 40%, 0.1);
+  border: 1px solid hsl(0, 100%, 40%);
+  color: hsl(0, 100%, 40%);
+
+  &:hover,
+  &:focus-visible {
+    background: hsl(0, 100%, 40%, 0.2);
+  }
+`;
