b5e1b84ee2a7a858816d9226bf8428faf163cf18
Rafał Solnica <granadag99@gmail.com>
2023-11-20 20:17:34 +0100

    added submit handler and implemented new styled components

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,47 @@
-import { StyledForm, Row, Button } from "./styled";
+import { useState } from "react";
+import {
+  StyledForm,
+  Row,
+  StyledButton,
+  RemoveButton,
+  StyledList,
+  ListItem,
+  Header,
+} from "./styled";
 
 export const Form = () => {
+  const [newItem, setNewItem] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTasks((currentTasks) => {
+      return [
+        ...currentTasks,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  };
+
+  console.log(tasks);
+
   return (
-    <StyledForm>
-      <Row>
-        <label htmlFor="item">Nowe zadanie</label>
-        <input type="text" id="item"></input>
-      </Row>
-      <Button>Dodaj</Button>
-    </StyledForm>
+    <>
+      <StyledForm onSubmit={handleSubmit}>
+        <Row>
+          <label htmlFor="item">Nowe zadanie</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          ></input>
+        </Row>
+        <StyledButton>Dodaj</StyledButton>
+      </StyledForm>
+      <Header>Lista zadań</Header>
+      <StyledList></StyledList>
+    </>
   );
 };
