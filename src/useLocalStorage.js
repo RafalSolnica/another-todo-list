596392577356d941ff9dcaba2087cb8fb268d9e4
Rafał Solnica <granadag99@gmail.com>
2023-11-21 21:19:06 +0100

    created useLocalStorage custom hook

---
+++ src/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = (keyName, initialValue) => {
+  const getInitialState = () =>
+    JSON.parse(localStorage.getItem(keyName)) || initialValue;
+
+  const [state, setState] = useState(getInitialState);
+
+  useEffect(() => {
+    localStorage.setItem(keyName, JSON.stringify(state));
+  }, [state, keyName]);
+
+  return [state, setState];
+};
